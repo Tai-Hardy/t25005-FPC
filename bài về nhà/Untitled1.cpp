48b2633754296f9f52df3ebee2316b3c96686569
Tai-Hardy <taiplus02082004@gmail.com>
2025-05-31 17:59:06 +0700

    bài tập hôm nay

---
+++ bài về nhà/Untitled1.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	printf("nhap a=");
+	scanf("%d",&a);
+	
+	int b;
+	printf("nhap b=");
+	scanf("%d",&b);
+	int c;
+	printf("nhap c");
+	scanf("%d",&c); 
+	  if(a+b>c){
+		printf("day la tam giac");
+	
+      }else{
+	   if(a+c>b){
+		printf("day la tam giac");
+	
+     }else{
+	  if(b+c>a){
+			printf("day la tam giac");
+		}else{
+			printf("khong phai tam giac");
+			 
+		} 
+	
+} 
+} 
+	}
+	
+
+
+	
+
---
+++ bài về nhà/Untitled3.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+	int x ;
+	printf("tinh tien dien cua x=");
+	scanf("%f",&x);
+	int y;
+	printf("tinh tien dien cua y=");
+	scanf("%f",&y);
+	int z;
+	printf("tinh tien dien cua z=");
+	scanf("%f",&z);
+	int t;
+	printf("tinh tien dien cua t=");
+	scanf("%f",&t);
+	if(x >= 200){
+	x == ( a-200)*2.536+100*2,014+50*1,736+50*1,678 ;
+	printf("so tien dien phai tra la");
+}else{
+	if(y >= 101){
+	y == (a-100)*2,014+50*1,736+50*1,678 ;
+}else{
+	if(z >= 51){
+	z==(a-51)*1,736+50*1,678;
+}else{
+	if(t <51){
+	t==a*1,678;
+}
+}
+}
+}
+}
+
+	
