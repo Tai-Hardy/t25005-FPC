d24d7e9ce299a62c663054f976fecad7e8f76e5d
Tai-Hardy <taiplus02082004@gmail.com>
2025-06-05 17:59:22 +0700

    bài tập ve nhà 6/5

---
+++ tinhtienthue.cpp
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main(){
+	int a;
+	printf("nhap a=");
+	scanf("%d",&a);
+	 
+	  
+	 	float x ;
+	 	
+
+	  if(a<=5){
+	   
+	  x=a*0.05;
+	  }else{ 
+	 if(a<=10){
+	  x=0.25+0.1*a;
+	  printf("so tien thue phai dong la:%f tr",x);
+	   
+	 }else{
+	 if(a<=18){
+	 x=0.75+0.15*a;
+	 
+	 
+	 
+	 	 printf("so tien thue phai dong la:%  f tr",x);
+	  
+	 }else{
+	 if(a<=32){
+	 	x=1.95+0.2*a; 
+	 	printf("so tien thue phai dong la:% f tr",x);
+		  
+	 
+	 }else{
+	 	if(a<=52){
+	 		x=4.75+0.25*a ;
+	 		printf("so tien thue phai dong la:% f tr",x); 
+	 }else{
+	 	if(a<=80){
+	 		x=9.75+0.3*a;
+	 		printf("so tien thue phai dong la:% f tr",x);
+			  
+			  	
+}
+}
+}
+}
+}
+}
+}
+
+
